612f09e0fdcf29cec501290cfeec03044569aaa2
Michael Guilaran <maykil093@gmail.com>
2025-04-26 07:10:06 +0800

    update contact form

--- components/contact-section.tsx
+++ components/contact-section.tsx
@@ -5,9 +5,51 @@ import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
 
 const Contact = () => {
     const [isClient, setIsClient] = useState(false);
+
+    const formSchema = z.object({
+        name: z
+            .string()
+            .min(2, { message: "Whoops! We need your name before you go on." })
+            .max(50),
+        email: z.string().email("Please enter a valid email address."),
+        message: z
+            .string()
+            .min(2, {
+                message:
+                    "Oops! Looks like you forgot to type a message. Say something!",
+            })
+            .max(50),
+    });
+
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+            email: "",
+            message: "",
+        },
+    });
+
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        console.log(values);
+        form.reset();
+    }
 
     useEffect(() => {
         setIsClient(true); // Run only on the client-side
@@ -16,6 +58,7 @@ const Contact = () => {
     if (!isClient) {
         return null; // Prevent rendering during SSR
     }
+
     return (
         <section
             id="contact"
@@ -30,58 +73,64 @@ const Contact = () => {
                 transition={{ duration: 0.5, delay: 0.1 }}
                 viewport={{ once: true }}
             >
-                <form method="POST" className="mt-4 space-y-4">
-                    <div className="flex flex-col">
-                        <label
-                            htmlFor="fullname"
-                            className="text-sm font-medium"
-                        >
-                            Full Name
-                        </label>
-                        <Input
-                            id="fullname"
+                <Form {...form}>
+                    <form
+                        onSubmit={form.handleSubmit(onSubmit)}
+                        className="space-y-8"
+                    >
+                        <FormField
+                            control={form.control}
                             name="name"
-                            type="text"
-                            required
-                            placeholder="Enter your full name"
-                            className="mt-2"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Name</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            placeholder="What should I call you?"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
                         />
-                    </div>
-
-                    <div className="flex flex-col">
-                        <label htmlFor="email" className="text-sm font-medium">
-                            Email
-                        </label>
-                        <Input
-                            id="email"
+                        <FormField
+                            control={form.control}
                             name="email"
-                            type="email"
-                            required
-                            placeholder="Enter your email"
-                            className="mt-2"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Email</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            placeholder="What's your email?"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
                         />
-                    </div>
-
-                    <div className="flex flex-col">
-                        <label
-                            htmlFor="message"
-                            className="text-sm font-medium"
-                        >
-                            Message
-                        </label>
-                        <Textarea
-                            id="message"
+                        <FormField
+                            control={form.control}
                             name="message"
-                            required
-                            placeholder="Enter your message"
-                            className="mt-2"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Message</FormLabel>
+                                    <FormControl>
+                                        <Textarea
+                                            placeholder="Type your message here..."
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
                         />
-                    </div>
-
-                    <Button type="submit" className="mt-4 w-full">
-                        Send Message
-                    </Button>
-                </form>
+                        <Button type="submit" className="mt-4 w-full">
+                            Send Message
+                        </Button>
+                    </form>
+                </Form>
             </motion.div>
         </section>
     );
